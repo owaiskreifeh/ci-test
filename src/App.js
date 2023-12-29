8b87f57b7f75a0374c836747feddce2bdeea894c
Owais Kreifeh <owaiskreifeh@gmail.com>
2023-12-30 01:15:55 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      CI/CD working
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
